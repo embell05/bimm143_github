3f014bd08619b16245ec726d284b1e4e6385502a
Emma Bell <embell@ucsd>
2026-02-25 12:47:18 -0800

    commiting class 5

---
+++ Class 05/Class05A.R
@@ -0,0 +1,15 @@
+#' ---
+#' title: "Class 4"
+#' author: "Emma Bell"
+#' date: "January 16th 2026"
+#' ---
+
+x <- 1:50
+# command return to get the script onto the console
+
+plot(x)
+plot(x, sin(x))
+plot(x, sin(x), typ="b")
+
+plot(x, sin(x), typ="l", col="blue", lwd=3, xlab="is it lunchtime yet?")
+
